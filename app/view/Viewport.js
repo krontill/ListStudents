7ea557c01f6503aa9f9ee22a3c365e0d770d70fb
krontill <till.work@gmail.com>
2014-12-04 20:49:44 +0300

    Add print button. Add binding group and title.

--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -8,11 +8,42 @@ Ext.define('ListStudents.view.Viewport', {
         align: 'stretch'
     },
 
+    viewModel: 'viewportModel',
+
     items: [
+        {
+            xtype: 'container',
+            layout: {
+                type: 'hbox',
+                align: 'stretch'
+            },
+
+            items: [
+                {
+                    xtype: 'displayfield',
+                    bind: '<h2>Список студентов {group}</h2>',
+                    padding: '0 10 0 50',
+                    flex: 1
+                },
+                {
+                    xtype: 'button',
+                    text: 'Распечатать',
+                    href: 'javascript:window.print(); void 0;',
+                    height: 31,
+                    margin: '5 10 15 20',
+                    width: 120
+                }
+            ]
+        },
+        {
+            xtype: 'displayfield',
+            bind: '<h2>{title}</h2>'
+        },
         {
             xtype: 'studentView'
 
-        },{
+        },
+        {
             xtype: 'lessonView'
         }
     ]
--- app/view/ViewportModel.js
+++ app/view/ViewportModel.js
@@ -1,4 +1,11 @@
 Ext.define('ListStudents.view.ViewportModel', {
-    extend: 'Ext.app.ViewModel'
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.viewportModel', // connects to viewModel/type below
+
+    data: {
+        title: 'ФИО первого студента в списке или мб null если не выбран?',
+        group: 'МТ-202'
+    }
 
 });
