7c458aca09294cd84b8a1633a2e1c1d29665e4ab
krontill <till.work@gmail.com>
2014-12-08 23:12:36 +0300

    Binding fio in alert savebutton.

--- app/view/student/StudentController.js
+++ app/view/student/StudentController.js
@@ -3,7 +3,20 @@ Ext.define('ListStudents.view.StudentController', {
     alias: 'controller.StudentController',
 
     onSaveChangeStudentClick: function () {
-        Ext.Msg.alert('Студент ххх изменен', 'Сохранили изменения!');
+        var studentView = Ext.ComponentQuery.query('studentView')[0];
+        var studentFio = studentView.getViewModel().getData();
+
+        if (studentFio.rec === null) {
+            Ext.Msg.alert('Студент не выбран', 'Ничего не изменилось!');
+        } else if (studentFio.rec.data === undefined) {
+            studentFio = studentFio.rec.name + ' ' + studentFio.rec.family + ' ' + studentFio.rec.patronymic;
+            Ext.Msg.alert('Новый студент ' + studentFio + ' сохранен', 'Сохранили изменения!');
+        } else {
+            studentFio = studentFio.rec.data.name + ' ' + studentFio.rec.data.family + ' ' + studentFio.rec.data.patronymic;
+            Ext.Msg.alert('Студент ' + studentFio + ' изменен', 'Сохранили изменения!');
+        }
+
+
     },
 
     onDeleteStudentClick: function () {
